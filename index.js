e9fad44a0824cfa1074e5bf8ff16798ecc94ff23
Ron Lynch <lynch.ronald1@gmail.com>
2018-06-24 01:51:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 // write your code below!
 
 var name = "Ron"
-var height = "66"
+var height = 66
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
 console.log(height)
+
 console.log(parseInt(height)+1)
